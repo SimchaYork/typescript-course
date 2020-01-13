58d82b3c19c7482c01469ae126ff3fb1e676ed17
Simcha Wood <truthfulthomas@gmail.com>
2020-01-13 13:35:57 -0500

    Classes

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,26 @@
-const button = document.querySelector('button');
+class Department {
+  private employees: string[] = [];
 
-if (button) {
-  button.addEventListener('click', () => {
-    console.log('Clicked!');
-  });
+  constructor(private readonly id: string, public name: string) {}
+
+  describe(this: Department) {
+    console.log(`Department: ${this.name} [${this.id}]`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
+
+const accounting = new Department('d1', 'Accounting');
+
+accounting.addEmployee('Tina');
+accounting.addEmployee('Musket');
+
+accounting.describe();
+accounting.printEmployeeInformation();
